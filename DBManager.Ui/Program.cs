d42963de61ffe1b6d94e99df1022689feebdac21
StarkShang <stark.shang@sjtu.edu.cn>
2018-04-20 12:14:00 +0800

    Start up the project DBManager. Create project DBManager.Core for core functions which are independent of UI. Create project DBManager.Ui for functions with UI

---
+++ DBManager.Ui/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using DBManager.Ui.ViewModels;
+using DBManager.Ui.Views;
+
+namespace DBManager.Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
